00f6d8ca8325edc21d24f54edb1d5ac4a1cb353d
Praveen <praveen@aurore.co.in>
2021-04-04 13:53:54 +0530

    Renamed search page to /

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,80 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import React, { useState, useEffect } from 'react';
+import { createClient } from 'pexels';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const client = createClient('563492ad6f917000010000011070a3625d3e4a8387879fdb8392db23')
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+import styles from './search.module.scss'
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+const pexels = () => {
+    const [images, setImages] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [pageImages, setPageImages] = useState([]);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    useEffect(() => {
+        const fetchPexelImages = async () => {
+            console.log('Fetching the pexel images')
+            const curatedPhotos = await client.photos.curated({ per_page: 8 });
+            setImages(curatedPhotos)
+            setPageImages(curatedPhotos.photos)
+        }
+        fetchPexelImages();
+        return () => {
+        };
+    }, []);
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    const search = async (event) => {
+        event.preventDefault();
+        const query = event.target.value;
+        const searchPhotos = await client.photos.search({ query: query, per_page: 8 })
+        setImages(searchPhotos)
+        setSearchQuery(query)
+        setPageImages(searchPhotos.photos)
+    }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    const loadMore = async (e) => {
+        e.preventDefault();
+        const pagination = {
+            page: images.page + 1,
+            per_page: images.per_page
+        }
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        const loadMore = await client.photos.curated(pagination)
+        setImages(loadMore)
+        setPageImages([...pageImages, ...loadMore.photos])
+        console.log(pageImages)
+    }
+    return (
+        <div>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h2>Pick It</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12 my-5">
+                        <input type="text" name="query" value={searchQuery} placeholder="Search Photos" onChange={e => { search(e) }} />
+                    </div>
+                </div>
+                <div className="row">
+                    {pageImages?.map(image => {
+                        return <div className="col-sm-12 col-md-3 my-2">
+                            <img className="img-fluid" style={{
+                                borderRadius: '10px',
+                                width: '100%'
+                            }} src={image?.src?.medium} alt="photo" />
+                        </div>
+                    })}
+                </div>
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-md-2 mt-5">
+                        <button className="btn btn-primary" onClick={e => loadMore(e)}>Load More</button>
+                    </div>
+                </div>
+            </div>
+            {/* {JSON.stringify(images)} */}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
+
+export default pexels;
